e9d13ae57f5a3ecabd20c653eb645890dc8e8c59
octopusRage <ahyana1995@gmail.com>
2016-11-14 14:45:57 -0500

    add handler if email not found when login

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -3,13 +3,21 @@ class Api::V1::Users::SessionsController < ApplicationController
     email = params['email']
     password = params['password']
     user = User.find_by(email: email)
-
-    if (user.valid_password? password) && !user.email.nil?
-      sign_in user, store: false
-      render json: {
-        status: 'success', 
-        data: user.credential_as_json
-      }, status: 200
+    if user
+      if (user.valid_password? password) && !user.email.nil?
+        sign_in user, store: false
+        render json: {
+          status: 'success', 
+          data: user.credential_as_json
+        }, status: 200
+      else
+        render json: {
+          status: 'fail',
+          data: {
+            messages: 'invalid username or password'
+          }
+        }, status: 422
+      end
     else
       render json: {
         status: 'fail',
